ad6732fc9368e4156350af35ea5d588d8462d20d
Ahmed <ahmed.a.qureshi@gmail.com>
2021-09-17 18:52:33 -0400

    Add PHP object

---
+++ Horizon Leviathan.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html>
+<title>Horizon Leviathan</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="productstyle.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.w3-sidebar a {font-family: "Roboto", sans-serif}
+body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
+</style>
+<body class="w3-content" style="max-width:1200px">
+
+<!-- Sidebar/menu -->
+<nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
+  <div class="w3-container w3-display-container w3-padding-16">
+    <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
+    <a href="eccomerce title.html" class="logo"><img src="images/horizonheader.png" alt=""></a>
+  </div>
+  <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
+    <a href="ecommerce prodpage 3.php" php class="w3-bar-item w3-button">Laptop</a>
+    <a href="ecommerce prodpage 2.php" class="w3-bar-item w3-button">Desktop</a>
+    <a href="ecommerce prodpage 1.php" class="w3-bar-item w3-button">Mobile</a>
+      <a href="javascript:void(0)" class="w3-bar-item w3-button" onclick="document.getElementById('newsletter').style.display='block'">Checkout</a>
+    </div>
+  <a href="#footer" class="w3-bar-item w3-button w3-padding">Contact</a>
+  <a href="#footer"  class="w3-bar-item w3-button w3-padding">Subscribe</a>
+
+</nav>
+
+<!-- Top menu on small screens -->
+<header class="w3-bar w3-top w3-hide-large w3-black w3-xlarge">
+  <div class="w3-bar-item w3-padding-24 w3-wide><img src="images/horizonheader.png"></div>
+  <a href="javascript:void(0)" class="w3-bar-item w3-button w3-padding-24 w3-right" onclick="w3_open()"><i class="fa fa-bars"></i></a>
+</header>
+
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-left:250px">
+
+  <!-- Push down content on small screens -->
+  <div class="w3-hide-large" style="margin-top:83px"></div>
+
+  <!-- Top header -->
+  <br></br>
+  <header class="w3-container w3-xlarge">
+    <p class="w3-left">Desktop</p>
+    <p class="w3-right">
+      <href="javascript:void(0)" class="fa fa-shopping-cart w3-margin-right" onclick="document.getElementById('newsletter').style.display='block'">
+    </p>
+  </header>
+
+  <div class="w3-container w3-text-grey" id="Mobile">
+    <p>8 items</p>
+  </div>
+
+  <!-- Product grid -->
+  <!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="product.css">
+  <script src="js/product.js"></script>
+    <body>
+        <div class="container">
+        	<div class="row">
+               <div class="col-xs-4 item-photo">
+                    <img src="images/desktop 1.jpg" width=350px/>
+                </div>
+                <div class="col-xs-5" style="border:0px solid gray">
+                    <h3>Horizon Leviathan</h3>
+                    <h5 style="color:#337ab7">Designed for you by <a href="#">Horizon</a> · <small style="color:#337ab7">(7034583 reviews)</small></h5>
+
+                    <h6 class="title-price"><small>MSRP</small></h6>
+                    <h3 style="margin-top:0px;">$ 1500</h3>
+
+                    <div class="section">
+                    </div>
+                    <div class="section" style="padding-bottom:5px;">
+                        <h6 class="title-attr"><small>Capacity</small></h6>
+                        <div>
+                            <div class="attr2">5 TB</div>
+                            <div class="attr2">10 TB</div>
+                        </div>
+                    </div>
+                    <div class="section" style="padding-bottom:20px;">
+                        <h6 class="title-attr"><small>Quantity</small></h6>
+                        <div>
+                            <input value="1" />
+                        </div>
+                    </div>
+
+                    <div class="section" style="padding-bottom:20px;">
+                        <href="javascript:void(0)" onclick="document.getElementById('newsletter').style.display='block'" button class="btn btn-success"><span style="margin-right:20px" class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Checkout</button>
+                    </div>
+                </div>
+
+                <div class="col-xs-9">
+                    <ul class="menu-items">
+                        <h4 class="active">Product Details</h4>
+
+                    </ul>
+                    <div style="width:100%;border-top:1px solid silver">
+                        <p style="padding:15px;">
+                            <small>
+                            The Horizon Leviathan is truly a supercomputer. It is the most powerful commercial PC on the maket. We take great pride in its performance and production to ensure it is able to perform even the most high strain and complex gaming and video and graphic proccessing tasks thrown at it.
+                            </small>
+                        </p>
+                        <small>
+                            <ul>
+                                <li>Intel Core i9 Processor 5.75 GHz</li>
+                                <br></br>
+                                <li>Quad Nvidia Gefore Titan X GPUs</li>
+                                <br></br>
+                                <li>128 GB RAM</li>
+                                <br></br>
+                                <li>9 TB HDD + 1 TB SSD</li>
+                                <br></br>
+                                <li>Horizon OS Version 10.7</li>
+                            </ul>
+                        </small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
+
+  <!-- Subscribe section -->
+  <div class="w3-container w3-black w3-padding-32">
+    <h1>Subscribe</h1>
+    <p>Enter your Email address below to get special offers updates on the latest products:</p>
+    <p><input class="w3-input w3-border" type="text" placeholder="Enter e-mail" style="width:100%"></p>
+    <button type="button" class="w3-button w3-red w3-margin-bottom">Subscribe</button>
+  </div>
+
+  <!-- Footer -->
+  <footer class="w3-padding-64 w3-light-grey w3-small w3-center" id="footer">
+    <div class="w3-row-padding">
+      <div class="w3-col s4">
+        <h4>Contact Us</h4>
+        <form action="/action_page.php" target="_blank">
+          <p><input class="w3-input w3-border" type="text" placeholder="Name" name="Name" required></p>
+          <p><input class="w3-input w3-border" type="text" placeholder="Email" name="Email" required></p>
+          <p><input class="w3-input w3-border" type="text" placeholder="Subject" name="Subject" required></p>
+          <p><input class="w3-input w3-border" type="text" placeholder="Message" name="Message" required></p>
+          <button type="submit" class="w3-button w3-block w3-black">Send</button>
+        </form>
+      </div>
+
+      <div class="w3-col s4">
+        <h4>Site Map</h4>
+        <p><a href="eccomerce title.html">Homepage</a></p>
+        <p><a href="about.html">About Us</a></p>
+        <p><a href="contact.php">Contact Us</a></p>
+      </div>
+
+      <div class="w3-col s4 w3-justify">
+        <h4>Horizon Product Support</h4>
+        <p><i class="fa fa-fw fa-map-marker"></i> 15 Central Blvd, Singapore 018969 </p>
+        <p><i class="fa fa-fw fa-phone"></i> +65 6471 7418</p>
+        <p><i class="fa fa-fw fa-envelope"></i> inquiries@horizonsystems.co.sg</p>
+        <h4>We accept</h4>
+        <p><i class="fa fa-fw fa-cc-amex"></i> Horizon Pay</p>
+        <p><i class="fa fa-fw fa-credit-card"></i> Credit Card</p>
+        <br>
+        <i class="fa fa-facebook-official w3-hover-opacity w3-large"></i>
+        <i class="fa fa-instagram w3-hover-opacity w3-large"></i>
+        <i class="fa fa-snapchat w3-hover-opacity w3-large"></i>
+        <i class="fa fa-pinterest-p w3-hover-opacity w3-large"></i>
+        <i class="fa fa-twitter w3-hover-opacity w3-large"></i>
+        <i class="fa fa-linkedin w3-hover-opacity w3-large"></i>
+      </div>
+    </div>
+  </footer>
+
+  <div class="w3-black w3-center w3-padding-24">&copy; 2022 Horizon Systems Limited | ALL RIGHTS RESERVED</a></div>
+
+  <!-- End page content -->
+</div>
+
+<!-- Newsletter Modal -->
+<div id="newsletter" class="w3-modal">
+  <div class="w3-modal-content w3-animate-zoom" style="padding:32px">
+    <div class="w3-container w3-white w3-center">
+      <i onclick="document.getElementById('newsletter').style.display='none'" class="fa fa-remove w3-right w3-button w3-transparent w3-xxlarge"></i>
+      <h2 class="w3-wide">Checkout</h2>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+
+      <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'>
+    <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+
+            <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+      <div class="container">
+    <div class="row">
+    <!-- You can make it whatever width you want. I'm making it full width
+    on <= small devices and 4/12 page width on >= medium devices -->
+    <div class="col-xs-8 col-md-8">
+
+
+    <!-- CREDIT CARD FORM STARTS HERE -->
+    <div class="panel panel-default credit-card-box">
+    <div class="panel-heading display-table" >
+    <div class="row display-tr" >
+    <h3 class="panel-title display-td" >Payment Details</h3>
+    <div class="display-td" >
+    <img class="img-responsive pull-left" src="http://i76.imgup.net/accepted_c22e0.png">
+    </div>
+    </div>
+    </div>
+    <div class="panel-body">
+    <form role="form" id="payment-form">
+    <div class="row">
+      <div class="col-xs-10">
+      <label for="couponCode">NAME ON CARD</label>
+      <input type="text" class="form-control" name="couponCode" />
+    <div class="col-xs-12">
+    <div class="form-group">
+    <label for="cardNumber">CARD NUMBER</label>
+    <div class="input-group">
+    <input
+    type="tel"
+    class="form-control"
+    name="cardNumber"
+    placeholder="Valid Card Number"
+    autocomplete="cc-number"
+    required autofocus
+    />
+    <span class="input-group-addon"><i class="fa fa-credit-card"></i></span>
+    </div>
+    </div>
+    </div>
+    </div>
+    <div class="row">
+    <div class="col-xs-5 col-md-4">
+    <div class="form-group">
+    <label for="cardExpiry"><span class="hidden-xs">EXPIRATION</span><span class="visible-xs-inline">EXP</span> DATE</label>
+    <input
+    type="tel"
+    class="form-control"
+    name="cardExpiry"
+    placeholder="MM / YY"
+    autocomplete="cc-exp"
+    required
+    />
+    </div>
+    </div>
+    <div class="col-xs-4 col-md-3 pull-left">
+    <div class="form-group">
+    <label for="cardCVC">CV CODE</label>
+    <input
+    type="tel"
+    class="form-control"
+    name="cardCVC"
+    placeholder="CVC"
+    autocomplete="cc-csc"
+    required
+    />
+    </div>
+    </div>
+    </div>
+    <div class="row">
+    <div class="col-xs-12">
+    <div class="form-group">
+    </div>
+    </div>
+    </div>
+    <div class="row">
+    <div class="col-xs-12">
+    <button class="btn btn-success btn-lg btn-block" type="submit">Confirm</button>
+    </div>
+    </div>
+    <div class="row" style="display:none;">
+    <div class="col-xs-12">
+    <p class="payment-errors"></p>
+    </div>
+    </div>
+    </form>
+    </div>
+    </div>
+    <!-- CREDIT CARD FORM ENDS HERE -->
+
+
+    </div>
+
+
+
+    </div>
+    </div>
+
+      <!-- If you're using Stripe for payments -->
+    <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+
+    </body>
+      <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js'></script>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery.payment/1.2.3/jquery.payment.min.js'></script>
+
+        <script src="js/index.js"></script>
+
+    </body>
+    </div>
+  </div>
+</div>
+
+<script>
+// Accordion
+function myAccFunc() {
+    var x = document.getElementById("demoAcc");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+// Click on the "Mobile" link on page load to open the accordion for demo purposes
+document.getElementById("myBtn").click();
+
+
+// Script to open and close sidebar
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+</script>
+
+</body>
+</html>
