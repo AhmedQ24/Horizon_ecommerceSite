0c551bdaa0e64602f71056dbf7d10c8752f4953e
Ahmed <ahmed.a.qureshi@gmail.com>
2021-09-17 18:44:57 -0400

    Contact Page
    A page created to provide the customer information required to contact the business.

---
+++ contact.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html>
+<title>Contact Horizon Systems</title>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/contactstyle.css" type="text/css">
+	<link rel="stylesheet" type="text/css" href="css/mobile.css">
+	<script src="js/mobile.js" type="text/javascript"></script>
+</head>
+<body>
+	<div id="page">
+		<div id="header">
+			<div>
+				<a href="eccomerce title.html" class="logo"><img src="images/horizon.png" alt=""></a>
+				<ul id="navigation">
+					<li class="selected">
+						<a href="eccomerce title.html">Home</a>
+					</li>
+					<li class="menu">
+						<a href="ecommerce prodpage 3.php">Store</a>
+					</li>
+					<li>
+						<a href="about.html">About</a>
+					</li>
+					<li class="menu">
+						<ul class="secondary">
+							<li>
+								<a href="singlepost.html">Single post</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="contact.php">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div id="body">
+			<div class="header">
+				<div class="contact">
+					<h1>Contact Us</h1>
+					<p>Fill out the form below if you have any questions, inquiries, or require product support. We will have a customer service representative contact you on your given email address as soon as possible.</p>
+					<form action="mail.php" method="POST">
+						<p>Name</p> <input type="text" name="name">
+						<p>Email</p> <input type="text" name="email">
+						<p>Message</p><textarea name="message" rows="6" cols="25"></textarea><br />
+						<input type="submit" value="Send"><input type="reset" value="Clear">
+
+					</form>
+				</div>
+			</div>
+		</div>
+
+		<div id="footer">
+			<div class="connect">
+				<div>
+					<h1>STAY UPDATED ON OUR PRODUCTS</h1>
+					<div>
+						<a href="http://facebook.com/" class="facebook">facebook</a>
+						<a href="http://www.twitter.com/" class="twitter">twitter</a>
+						<a href="http://googleplus.com/" class="googleplus">googleplus</a>
+						<a href="http://pinterest.com/" class="pinterest">pinterest</a>
+					</div>
+				</div>
+			</div>
+			<div class="footnote">
+				<div>
+					<p>&copy; 2022 Horizon Systems Limited | ALL RIGHTS RESERVED</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
